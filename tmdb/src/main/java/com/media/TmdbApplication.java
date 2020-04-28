22ebcf8fa0952326890f99a5a802e2deebd16fac
ankit bajaj <ankitbajaj64@gmail.com>
2020-04-28 21:31:42 +0530

    Data is being consumed by API now and being displayed on the console- Issue was to add JDK version in POM.xml

---
+++ tmdb/src/main/java/com/media/TmdbApplication.java
@@ -0,0 +1,18 @@
+package com.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import static com.media.client.CallRestService.callRestService;
+
+@SpringBootApplication
+@Component("com.media.client")
+public class TmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdbApplication.class, args);
+		callRestService();
+	}
+
+}
---
+++ tmdb/src/main/java/com/media/client/CallRestService.java
@@ -0,0 +1,36 @@
+package com.media.client;
+
+import net.minidev.json.JSONObject;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class CallRestService implements CommandLineRunner {
+
+    public static void callRestService(){
+        RestTemplate restTemplate= new RestTemplate();
+        JSONObject response = restTemplate.getForObject("https://api.themoviedb.org/3/search/company?api_key=433d425daefdff55eeb180ec5abfa479&query=Titanic", JSONObject.class);
+
+        try {
+            System.out.println("Response: "+response.toJSONString());
+            List<Map> list = (ArrayList) response.get("results");
+            for(Map l : list) {
+                Map<String, String> resultsMap = l;
+                System.out.println("Name: "+resultsMap.get("name"));
+                System.out.println("Origin Country: "+resultsMap.get("origin_country"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
